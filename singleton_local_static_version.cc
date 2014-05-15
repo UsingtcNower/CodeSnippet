a45194e7d3ff188680931e7e9a70e26be9c82614
nower <nowerzt@gmail.com>
2014-05-15 23:05:17 +0800

    add singleton_local_static_version

---
+++ singleton_local_static_version.cc
@@ -0,0 +1,40 @@
+// Singleton pattern code snippet 
+#include "pattern.h"
+#include <iostream>
+#include <memory>
+
+namespace pattern{
+
+class SingletonCounter {
+private:
+	SingletonCounter():cnt(0) {}
+	int cnt;
+	DISALLOW_EVIL_CONSTRUCTORS(SingletonCounter);
+public:
+	static SingletonCounter& getInstance() {
+		static SingletonCounter instance;
+		return instance;
+	}
+
+	void increase(int x) {
+		cnt += x;
+	}
+
+	int getCount() {
+		return cnt;
+	}
+};
+
+}	// namespace pattern
+
+
+int
+main()
+{
+	pattern::SingletonCounter user1 = pattern::SingletonCounter::getInstance();
+	user1.increase(1);
+	std::cout<<user1.getCount()<<std::endl;
+	pattern::SingletonCounter user2 = pattern::SingletonCounter::getInstance();
+	user2.increase(2);
+	std::cout<<user2.getCount()<<std::endl;
+}
