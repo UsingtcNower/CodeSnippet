4f6dee56989251147cd6c31317128eb24a348d95
tao zhang <nowerzt@gmail.com>
2016-08-08 09:03:22 +0800

    update

---
+++ common.h
@@ -0,0 +1,26 @@
+#ifndef _COMMON_H
+namespace blocking_timer
+{
+	int main();
+}
+namespace two_async_op
+{
+	int main();
+}
+namespace multhread_async_op
+{
+	int main();
+}
+namespace web_client
+{
+	int main();
+}
+namespace udp_receiver
+{
+	int main();
+}
+namespace this_pointer
+{
+	int main();
+}
+#endif //_COMMON_H
