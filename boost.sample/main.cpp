61f9c750a71fd48f83e653224028f11623c077e7
tao zhang <nowerzt@gmail.com>
2016-10-28 10:39:39 +0800

    update

--- boost.sample/main.cpp
+++ boost.sample/main.cpp
@@ -1,6 +1,29 @@
 #include "common.h"
+#include <iostream>
+
+class Base
+{
+public:
+	Base() {}
+	virtual ~Base() {}
+};
+
+class Father : public Base
+{
+public:
+	Father() {}
+	~Father() {}
+};
+
+class Child : public Father
+{
+public:
+	Child() {}
+	~Child() {}
+};
 
 int main()
 {
-	two_async_op::main();
+	Base* base = new Child();
+	//blocking_timer::main();
 }
